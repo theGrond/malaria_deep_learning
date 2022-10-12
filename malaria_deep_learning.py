00506a15fa2e0671d8922f08ba45a0a29bb0774a
“theGrond” <gohlke.max@gmx.de>
2022-10-12 18:29:36 +0200

    first model but bad accuracy

--- malaria_deep_learning.py
+++ malaria_deep_learning.py
@@ -7,8 +7,18 @@ from collections import Counter
 import cv2
 from concurrent import futures
 import threading
+import matplotlib.pyplot as plt
+# import tensorflow as tf
+import datetime
+
+import tensorflow.compat.v1 as tf 
+
+tf.enable_eager_execution(tf.ConfigProto(log_device_placement=True)) 
+
+#%matplotlib inline
 
 # load dataset
+print("\nload dataset\n--------------------------")
 base_dir = os.path.join('./cell_images')
 infected_dir = os.path.join(base_dir,'Parasitized')
 healthy_dir = os.path.join(base_dir,'Uninfected')
@@ -17,9 +27,11 @@ infected_files = glob.glob(infected_dir+'/*.png')
 healthy_files = glob.glob(healthy_dir+'/*.png')
 
 # print length of folders
+print("\nsize of folders\n--------------------------")
 print(len(infected_files), len(healthy_files))
 
 #build data frame
+print("\ndata frame\n--------------------------")
 np.random.seed(42)
 
 files_df = pd.DataFrame({
@@ -29,10 +41,9 @@ files_df = pd.DataFrame({
 
 # show data frame
 print(files_df.head())
-print()
 
 # build and explore image datasets
-
+print("\nbuild and explore image datasets\n--------------------------")
 train_files, test_files, train_labels, test_labels = train_test_split(files_df['filename'].values,
                                                                       files_df['label'].values,
                                                                       test_size=0.3, random_state=42)
@@ -42,9 +53,9 @@ train_files, val_files, train_labels, val_labels = train_test_split(train_files,
 
 print(train_files.shape, val_files.shape, test_files.shape)
 print('Train:', Counter(train_labels), '\nVal:', Counter(val_labels), '\nTest:', Counter(test_labels))
-print()
 
 # summary statistics
+print("\nsummary statistics\n--------------------------")
 def get_img_shape_parallel(idx, img, total_imgs):
     if idx % 5000 == 0 or idx == (total_imgs - 1):
         print('{}: working on img num: {}'.format(threading.current_thread().name,
@@ -63,3 +74,169 @@ print('Min Dimensions:', np.min(train_img_dims, axis=0))
 print('Avg Dimensions:', np.mean(train_img_dims, axis=0))
 print('Median Dimensions:', np.median(train_img_dims, axis=0))
 print('Max Dimensions:', np.max(train_img_dims, axis=0))
+
+# load up and resize images to fixed dimensions
+print("\nload up and resize images to fixed dimensions\n--------------------------")
+IMG_DIMS = (80, 80)
+
+def get_img_data_parallel(idx, img, total_imgs):
+    if idx % 5000 == 0 or idx == (total_imgs - 1):
+        print('{}: working on img num: {}'.format(threading.current_thread().name,
+                                                  idx))
+    img = cv2.imread(img)
+    img = cv2.resize(img, dsize=IMG_DIMS,
+                     interpolation=cv2.INTER_CUBIC)
+    img = np.array(img, dtype=np.float32)
+    return img
+
+ex = futures.ThreadPoolExecutor(max_workers=None)
+train_data_inp = [(idx, img, len(train_files)) for idx, img in enumerate(train_files)]
+val_data_inp = [(idx, img, len(val_files)) for idx, img in enumerate(val_files)]
+test_data_inp = [(idx, img, len(test_files)) for idx, img in enumerate(test_files)]
+
+print('\nLoading Train Images:')
+train_data_map = ex.map(get_img_data_parallel,
+                        [record[0] for record in train_data_inp],
+                        [record[1] for record in train_data_inp],
+                        [record[2] for record in train_data_inp])
+train_data = np.array(list(train_data_map))
+
+print('\nLoading Validation Images:')
+val_data_map = ex.map(get_img_data_parallel,
+                        [record[0] for record in val_data_inp],
+                        [record[1] for record in val_data_inp],
+                        [record[2] for record in val_data_inp])
+val_data = np.array(list(val_data_map))
+
+print('\nLoading Test Images:')
+test_data_map = ex.map(get_img_data_parallel,
+                        [record[0] for record in test_data_inp],
+                        [record[1] for record in test_data_inp],
+                        [record[2] for record in test_data_inp])
+test_data = np.array(list(test_data_map))
+
+print(train_data.shape, val_data.shape, test_data.shape)
+
+# view sample cell images
+print("\nview sample cell images\n--------------------------")
+plt.figure(1 , figsize = (8 , 8))
+n = 0
+for i in range(16):
+    n += 1
+    r = np.random.randint(0 , train_data.shape[0] , 1)
+    plt.subplot(4 , 4 , n)
+    plt.subplots_adjust(hspace = 0.5 , wspace = 0.5)
+    plt.imshow(train_data[r[0]]/255.)
+    plt.title('{}'.format(train_labels[r[0]]))
+    plt.xticks([]) , plt.yticks([])
+
+plt.show()
+
+# set up configuration settings
+print("\nset up configuration settings\n--------------------------")
+
+BATCH_SIZE = 64
+NUM_CLASSES = 2
+EPOCHS = 25
+INPUT_SHAPE = (80, 80, 3)
+
+train_imgs_scaled = train_data / 255.
+val_imgs_scaled = val_data / 255.
+
+# encode text category labels
+from sklearn.preprocessing import LabelEncoder
+
+le = LabelEncoder()
+le.fit(train_labels)
+train_labels_enc = le.transform(train_labels)
+val_labels_enc = le.transform(val_labels)
+
+print(train_labels[:6], train_labels_enc[:6])
+
+# use tensorflow
+print("\nuse tensorflow\n--------------------------")
+
+# tf.random.set_seed(42)
+tf.set_random_seed(
+    42
+)
+
+print("Tensorflow-Version:"+tf.__version__)
+
+# CNN
+print("\nDefine CNN architecture\n--------------------------")
+
+inp = tf.keras.layers.Input(shape=INPUT_SHAPE)
+
+conv1 = tf.keras.layers.Conv2D(32, kernel_size=(3, 3),
+                               activation='relu', padding='same')(inp)
+pool1 = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))(conv1)
+conv2 = tf.keras.layers.Conv2D(64, kernel_size=(3, 3),
+                               activation='relu', padding='same')(pool1)
+pool2 = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))(conv2)
+conv3 = tf.keras.layers.Conv2D(128, kernel_size=(3, 3),
+                               activation='relu', padding='same')(pool2)
+pool3 = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))(conv3)
+
+flat = tf.keras.layers.Flatten()(pool3)
+
+hidden1 = tf.keras.layers.Dense(512, activation='relu')(flat)
+drop1 = tf.keras.layers.Dropout(rate=0.3)(hidden1)
+hidden2 = tf.keras.layers.Dense(512, activation='relu')(drop1)
+drop2 = tf.keras.layers.Dropout(rate=0.3)(hidden2)
+
+out = tf.keras.layers.Dense(1, activation='sigmoid')(drop2)
+
+model = tf.keras.Model(inputs=inp, outputs=out)
+model.compile(optimizer='adam',
+                loss='binary_crossentropy',
+                metrics=['accuracy'])
+model.summary()
+
+# train model
+print("\ntrain model\n--------------------------")
+
+logdir = os.path.join('/home/dipanzan_sarkar/projects/tensorboard_logs',
+                      datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
+tensorboard_callback = tf.keras.callbacks.TensorBoard(logdir, histogram_freq=1)
+reduce_lr = tf.keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.5,
+                              patience=2, min_lr=0.000001)
+callbacks = [reduce_lr, tensorboard_callback]
+
+history = model.fit(x=train_imgs_scaled, y=train_labels_enc,
+                    batch_size=BATCH_SIZE,
+                    epochs=EPOCHS,
+                    validation_data=(val_imgs_scaled, val_labels_enc),
+                    callbacks=callbacks,
+                    verbose=1)
+
+# plotting training and validation accuracy and loss curves
+print("\nplotting training and validation accuracy and loss curves\n--------------------------")
+
+f, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 4))
+t = f.suptitle('Basic CNN Performance', fontsize=12)
+f.subplots_adjust(top=0.85, wspace=0.3)
+
+max_epoch = len(history.history['accuracy'])+1
+epoch_list = list(range(1,max_epoch))
+ax1.plot(epoch_list, history.history['accuracy'], label='Train Accuracy')
+ax1.plot(epoch_list, history.history['val_accuracy'], label='Validation Accuracy')
+ax1.set_xticks(np.arange(1, max_epoch, 5))
+ax1.set_ylabel('Accuracy Value')
+ax1.set_xlabel('Epoch')
+ax1.set_title('Accuracy')
+l1 = ax1.legend(loc="best")
+
+ax2.plot(epoch_list, history.history['loss'], label='Train Loss')
+ax2.plot(epoch_list, history.history['val_loss'], label='Validation Loss')
+ax2.set_xticks(np.arange(1, max_epoch, 5))
+ax2.set_ylabel('Loss Value')
+ax2.set_xlabel('Epoch')
+ax2.set_title('Loss')
+l2 = ax2.legend(loc="best")
+plt.show()
+
+# save model
+print("\nsave model\n--------------------------")
+
+model.save('basic_cnn.h5')
